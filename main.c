833b2d787f8b513190de53547683594dbd1d0ae6
yuminchoi <chldbals219@naver.com>
2018-11-22 15:34:16 +0900

    third sub.

--- main.c
+++ main.c
@@ -4,19 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[100]="Programming course";
-	//char dst[100];
-	int i=0;
 	
-	while(src[i]!='\0')
+	FILE *fp;
+	char input[100];
+	int i;
+	
+	fp=fopen("sample.txt", "w");
+	
+	for(i=0;i<3;i++)
 	{
-		//dst[i]=src[i];
-		i++;
+		printf("input a word : ");
+		scanf("%s", input);
+		fprintf(fp, "%s\n", input);
 	}
-	//dst[i]='\0';
 	
-	//printf("dst: %s\n", dst);
-	printf("%s (%i)\n", src, i);
+	
+	fclose(fp); 
 	
 	return 0;
 }

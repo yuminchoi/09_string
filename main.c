dab50367a0fca9df5fa4b510ef05b7fc1bf446f7
yuminchoi <chldbals219@naver.com>
2018-11-22 16:35:49 +0900

    last sub.

--- main.c
+++ main.c
@@ -6,30 +6,29 @@
 
 int main(int argc, char *argv[]) {
 	
-	FILE *fp, *fp2;
-	char input;
-	char original[100];
-	char copy[100];
-	int i=0;
+	FILE *fp;	
+	char word[100];
+	char filename[100];
+	char input[100];
 	
-	printf("original file: ");
-	scanf("%s", original);
-	printf("copy file: ");
-	scanf("%s", copy);
+	printf("file name: ");
+	scanf("%s", filename);
 	
-	fp=fopen(original, "r");
-	fp2=fopen(copy, "w");
+	fp=fopen(filename, "r");
 	
-	while((input=fgetc(fp))!=EOF)
+	printf("input a word to find: ");
+	scanf("%s", word);
+	
+	while(fgets(input, 100, fp)!=NULL)
 	{
-		fputc(input, fp2);
-		i++;
-	} 
+		if(strncmp(input, word, strlen(word))==0)
+		//if(strcmp(input, word)==0)
+		{
+			printf("find a word %s\n", input);
+		}
+	}
 	
-	printf("Copy succeed! (%i Bytes copied)\n", i);
-	
-	fclose(fp);
-	fclose(fp2);
+	printf("search done!\n");
 	
 	return 0;
 }
